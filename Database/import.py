b06669b93030d329048217cd1e63d11f6b33a6db
kristianeo <olsen.kristiane@gmail.com>
2025-11-19 23:53:34 +0100

    Update import.py
    Added code for inserting into questions table.

--- Database/import.py
+++ Database/import.py
@@ -1,3 +1,5 @@
+from tkinter.constants import INSERT
+
 import pymysql #Python library for MySQL queries
 import json
 from dotenv import load_dotenv
@@ -18,10 +20,17 @@ with (open('questions.json', 'r') as qj):
 cursor = mydb.cursor()
 
 game_number = 0
-for i in data["games"]:
+for game in data["games"]:
     game_number += 1
     cursor.execute("INSERT INTO QuizzerQuestions.games(name) VALUES (%s)",
-                   f'Game {game_number}',)
+                   f'Game {game_number}', )
+    game_id = cursor.lastrowid
+
+    for q in game["questions"]:
+        cursor.execute("INSERT INTO QuizzerQuestions.questions(gameID, question, correct_answer) VALUES (%s, %s, %s)",
+                       (game_id, q["question"], q["correct"])
+                       )
+
 
 mydb.commit()
 cursor.close()
