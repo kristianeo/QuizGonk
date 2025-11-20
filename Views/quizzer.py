7cd1cb61725cc5bdf6dbb1ba6a5242983144fb1f
kristianeo <olsen.kristiane@gmail.com>
2025-11-20 19:16:30 +0100

    Added function to retrieve questions/answers/correct index list

--- Views/quizzer.py
+++ Views/quizzer.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from Database.import_games import *
 load_dotenv("../.env")
 
@@ -11,69 +13,41 @@ mydb = pymysql.connect(
 with (open('questions.json', 'r') as qj):
     data = json.load(qj)
 
-def get_questions(selected_game, mydb):
+def get_question_list(selected_game, mydb):
     cursor = mydb.cursor()
     query = """
-            SELECT question, correct_answer
+            SELECT option_text, options.questionID
+            FROM QuizzerQuestions.options
+                     JOIN QuizzerQuestions.questions ON options.questionID = questions.questionID
+            WHERE gameID = %s
+            """
+    cursor.execute(query, (selected_game,))
+    alternatives = cursor.fetchall()
+    cursor.close()
+
+    alt_list = defaultdict(list)
+    for k, v in alternatives:
+        alt_list[v].append(k)
+
+    cursor = mydb.cursor()
+    query = """
+            SELECT question, correct_answer, questionID
             FROM QuizzerQuestions.questions
             WHERE gameID = %s
             """
     cursor.execute(query, (selected_game,))
     questions = cursor.fetchall()
     cursor.close()
-    q_and_right_answer = []
-    for q in questions:
-        q_and_right_answer.append(q)
-    print(q_and_right_answer)
 
-def get_alternatives(selected_game, mydb):
-    cursor = mydb.cursor()
-    query = """
-            SELECT option_text, option_index
-            FROM QuizzerQuestions.options
-            JOIN QuizzerQuestions.questions ON options.questionID = questions.questionID
-            WHERE gameID = %s
-            """
-    cursor.execute(query, (selected_game,))
-    alternatives = cursor.fetchall()
-    alternatives_and_indices = []
-    cursor.close()
-    for a in alternatives:
-        alternatives_and_indices.append(a)
-    print(alternatives_and_indices)
+    quest_list = defaultdict(list)
+    right_index_list = defaultdict(list)
+    for q, r, s in questions:
+        quest_list[s].append(q)
+        right_index_list[s].append(r)
 
-def game_loop(selected):
-    selected_game = selected
-    print("Let's test your knowledge!")
-    get_questions(selected_game, mydb)
-    get_alternatives(selected_game, mydb)
+    final_list = defaultdict(list)
+    for x in (quest_list, right_index_list, alt_list):
+        for key, value in x.items():
+            final_list[key].append(value)
 
-#
-#
-# right_wrong = []
-# answers = []
-
-game_loop(3)
-
-
-
-    #Get selected game
-    #getQuestions(selected_game)
-        #Returns questions
-    #getAlternatives()
-    #getCorrectIDX()
-    ### Questions =[[spørsmål1,[alt1, alt2, alt3, alt4],[2]], Spørsmål2, [Alternativer],[riktig svar] ]
-    #answers = []
-    # right_wrong = ["right", "wrong", "right", "wrong"]
-    #
-    # for each question in questions():
-        # correct.append(question[1])
-        # print(question[0])
-        #   for each alt in question[1]
-        # selected_answer_idx = int(input("Velg et svar"))-1
-        # answers.append(selected_answer_idx)
-        # if selected_answer_idx == question[2]:
-        # right_wrong.append("right")
-        # else right_wrong.append("wrong")
-    #resultScreen(selected_game, answers, correct)
-    #valid inputs xX/ qQ for quit, 1-4 for svar
+    print(list(final_list.values()))
