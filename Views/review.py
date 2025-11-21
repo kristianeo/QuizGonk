d551504b5470582bfc5a5ca8dbbe95332c2bdfdf
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2025-11-21 19:58:25 +0100

    did some work on review

--- Views/review.py
+++ Views/review.py
@@ -1,9 +1,41 @@
 def show_review(handler):
     print("Review")
 
-    wait_for_user = True
-    while wait_for_user:
-        user_input = input("Type 'end' to finish the review: ").strip().lower()
-        if user_input == 'end':
-            wait_for_user = False
-    handler.navigate_to("main")
+    user_answers = handler.user_answers
+    right_wrong = handler.results_data
+    correct_index = handler.review_data
+    questions = handler.quiz_game_data
+
+    print(questions)
+
+    indices_to_remove = [i for i, val in enumerate(right_wrong) if val == "right"]
+    for i in reversed(indices_to_remove):
+        user_answers.pop(i)
+        correct_index.pop(i)
+        questions.pop(i)
+        right_wrong.pop(i)
+        
+    current = 0
+    while True:
+        print("You got ", len(right_wrong), " questions wrong!")
+        print("Let's review them")
+
+        while 0 <= current < len(questions):
+            print("\nQuestion {}: {}".format(current + 1, questions[current]['question']))
+            for index, options in enumerate(questions[current]['answers']):
+                marker = ""
+                if index == correct_index[current]:
+                    marker += "<--- Correct"
+                if index == user_answers[current]:
+                    if marker:
+                        marker += " | "
+                    marker += "<--- You answered"
+                print("  {}. {} {}".format(index + 1, answer, marker))
+            user_input = input("Enter 'n' for next, '+' for previous, 'x' or 'q' to exit review: ").strip().lower()
+            if user_input == "n":
+                current += 1
+            elif user_input == "+":
+                current -= 1
+            elif user_input in ("x", "q"):
+                handler.navigate_to('main')
+                return
