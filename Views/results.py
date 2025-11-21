6083a038fa19ce3f0dafad9f66dc0c6a958651f8
kristianeo <olsen.kristiane@gmail.com>
2025-11-21 19:50:40 +0100

    added result screen.

--- Views/results.py
+++ Views/results.py
@@ -1,9 +1,33 @@
 def show_results(handler):
-    print("Results Summary")
+    results_data_check = handler.results_data
+
+    right_count = 0
+    wrong_count = 0
+
+    for results in results_data_check:
+        if results == "wrong":
+            wrong_count += 1
+        else:
+            right_count += 1
+
+    accuracy = (right_count/(right_count + wrong_count))*100
+    formatted_accuracy = f"{accuracy:.2f}"
+
+    print(f"\nResults Summary:\n\nRight answers: {right_count}\nWrong answers: "
+          f"{wrong_count}\nAccuracy: {formatted_accuracy}%\n")
+
 
     wait_for_user = True
     while wait_for_user:
-        user_input = input("Type 'end' to finish the game: ").strip().lower()
-        if user_input == 'end':
-            wait_for_user = False
-    handler.navigate_to("review")
+        try:
+            user_input = input("Do you want a review? (y/n): ").strip().lower()
+            if user_input == 'y':
+                handler.navigate_to("review")
+                wait_for_user = False
+            if user_input == 'n':
+                handler.navigate_to("main")
+                wait_for_user = False
+            else:
+                print("Please select yes for review, or chose no to go back to main menu.")
+        except ValueError:
+            print("Please select yes for review, or chose no to go to main menu.")
